867bd1a9e41571290870b388e75cdcfada7dffbe
tinabeattysmith <tinatest2020@gmail.com>
2020-10-26 17:07:54 -0400

    Tina fetch added

--- src/components/Zomato.jsx
+++ src/components/Zomato.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
-function Zomato() {
+const Zomato = (props) => {
+
+  const fetchZomato = () => {
+    fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=${props.latitude}&lon=${props.latitude}`, {
+    method: 'GET',
+    headers: new Headers ({
+      'Accept': 'application/json',
+      'user-key': 'cdaccae4e26b9c93178bb638864824b4'  
+    })
+  }).then( (res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })   
+}
+  
   return (
 
     <div>
