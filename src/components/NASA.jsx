d9640d0647c5b9ad36131e963106b87478648bc8
Jonny Schult <jonathon.schult@gmail.com>
2020-10-26 18:22:55 -0500

    yoda

--- src/components/NASA.jsx
+++ src/components/NASA.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-function NASA() {
+let key = `vuG7MCOAO8PDyiw8H7QWJULgngWRc9lv1eYeWzt1`
+let baseURL = `https://api.nasa.gov/planetary/earth/assets?`;
+let date = new Date().toISOString().split('T')[0]
+
+function NASA(props) {
+  const fetcherFunction = () => {
+    fetch(`${baseURL}lon=${props.longitude}&lat=${props.lattitude}&date=${date}&api_key=${key}`)
+  }
+
   return (
 
     <div>
--- src/components/Zomato.jsx
+++ src/components/Zomato.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-
-function Zomato() {
-  return (
-
-    <div>
-    </div>
-
-  );
-}
-
+import React, { useEffect } from "react";
+import { Card, CardText, CardBody } from "reactstrap";
+const Zomato = (props) => {
+  const fetchZomato = () => {
+    fetch(
+      `https://developers.zomato.com/api/v2.1/geocode?lat=39.688699&lon=-86.06519`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/json",
+          "user-key": "cdaccae4e26b9c93178bb638864824b4",
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        // data.nearby_restaurants.slice(0, 3);
+        console.log(data.nearby_restaurants.slice(0, 3));
+      });
+  };
+  useEffect(() => {
+    fetchZomato();
+  }, []);
+  return <div></div>;
+};
 export default Zomato;
