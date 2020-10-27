7de23fa1f63ae44d4cde7666f675b1d970be6210
theMagnificentJay <reedjoshua408@gmail.com>
2020-10-27 10:07:44 -0500

    pushpushlastday

--- src/components/OpenWeather.jsx
+++ src/components/OpenWeather.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "reactstrap";
 
+const baseUrl = "http://api.openweathermap.org/data/2.5/weather";
 const key = "be197d61a8d4d154bdb54d626e41a007";
 
 const OpenWeather = (props) => {
@@ -12,7 +13,7 @@ const OpenWeather = (props) => {
 
   const fetchOpenWeather = () => {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&units=${unit}&appid=${key}`
+      `${baseUrl}?lat=${props.latitude}&lon=${props.longitude}&units=${unit}&appid=${key}`
     )
       .then((res) => res.json())
       .then((res) => {
@@ -35,10 +36,10 @@ const OpenWeather = (props) => {
       {/* <img
         src={`http://openweathermap.org/img/w/${weatherWeather[0].icon}.png`}
         alt=""
-      /> */}
-      {/* <p>Weather in your area is {weatherWeather[0].description}.</p> */}
-      <p>High of {weatherMain.temp_max}&#176;</p>
-      <p>Low of {weatherMain.temp_min}&#176;</p>
+      />
+      <p> Weather in your area is {weatherWeather[0].description}.</p> */}
+      <p>Temperature is between {weatherMain.temp_max}&#176; and {weatherMain.temp_min}&#176;</p>
+      <p></p>
       <p>Feels like {weatherMain.feels_like}&#176;</p>
       <div style={{ margin: "auto", marginTop: "50px", width: "65%" }}>
         <Button
