fac8de7a3046a2fbf78c631f5e34b6d509ec4547
theMagnificentJay <reedjoshua408@gmail.com>
2020-10-26 19:19:34 -0500

    push

--- src/components/NASA.jsx
+++ src/components/NASA.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
-function NASA(props) {
-    console.log('lat:' + props.latitude, 'lon:' + props.longitude);
-  return (
+const baseURL = 'https://api.nasa.gov/planetary/earth/assets';
+const key = 'y3DUrejxMPFNOGtCKPo36l4yaBgHPoNb8kInsRLw';
+
+const NASA = (props) => {
+  console.log(props.longitude);
+  console.log(props.latitude);
+
+  const fetchResults = () => {
+    let url = `${baseURL}?lon=${props.longitude}&lat=${props.latitude}&api_key=${key}`;
+
+    fetch(url)
+   .then(res => res.json())
+   .then(data => setResults(data.response.docs))
+   .catch(err => console.log(err));
+ };
+
+  return (   
 
     <div>
     </div>
