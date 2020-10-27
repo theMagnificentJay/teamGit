b920c52ff8bbe41337930a4ab5e0faaeb9e92daf
Jonny Schult <jonathon.schult@gmail.com>
2020-10-26 20:50:48 -0500

    push before pull.

--- src/components/NASA.jsx
+++ src/components/NASA.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+// import React from 'react';
 
-let key = `vuG7MCOAO8PDyiw8H7QWJULgngWRc9lv1eYeWzt1`
-let baseURL = `https://api.nasa.gov/planetary/earth/assets?`;
-let date = new Date().toISOString().split('T')[0]
+// let key = `vuG7MCOAO8PDyiw8H7QWJULgngWRc9lv1eYeWzt1`
+// let baseURL = `https://api.nasa.gov/planetary/earth/assets?`;
+// let date = new Date().toISOString().split('T')[0]
 
-function NASA(props) {
-  const fetcherFunction = () => {
-    fetch(`${baseURL}lon=${props.longitude}&lat=${props.lattitude}&date=${date}&api_key=${key}`)
-  }
+// function NASA(props) {
+//   const fetcherFunction = () => {
+//     fetch(`${baseURL}lon=${props.longitude}&lat=${props.lattitude}&date=${date}&api_key=${key}`)
+//   }
 
-  return (
+//   return (
 
-    <div>
-    </div>
+//     <div>
+//     </div>
 
-  );
-}
+//   );
+// }
 
-export default NASA;
+// export default NASA;
--- src/components/Zomato.jsx
+++ src/components/Zomato.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardText, CardBody } from "reactstrap";
 const Zomato = (props) => {
+  const [restOne, setRestOne] = useState("");
+  const [restTwo, setRestTwo] = useState("");
+  const [restThree, setRestThree] = useState("");
+
   const fetchZomato = () => {
     fetch(
       `https://developers.zomato.com/api/v2.1/geocode?lat=39.688699&lon=-86.06519`,
@@ -15,8 +19,11 @@ const Zomato = (props) => {
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
-        // data.nearby_restaurants.slice(0, 3);
+        const restaurants = data.nearby_restaurants.slice(0, 3);
         console.log(data.nearby_restaurants.slice(0, 3));
+        console.log(restaurants[0].restaurant.name);
+        setRestOne(restaurants[0].restaurant.name);
+        console.log(restOne);
       });
   };
   useEffect(() => {
