148338e763bc0b95e9fd32bdd0a7206eb6b8d9e8
tinabeattysmith <tinatest2020@gmail.com>
2020-10-26 22:50:52 -0400

    just an update

--- src/components/Zomato.jsx
+++ src/components/Zomato.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+//import {} from 'reactstrap';
 
 const Zomato = (props) => {
+  const [restOne, setRestOne]=useState('');
+  const [restTwo, setRestTwo]=useState('');
+  const [restThree, setRestThree]=useState('');
 
   const fetchZomato = () => {
-    fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=${props.latitude}&lon=${props.latitude}`, {
+   
+    // TODO:replace lat and lon values with prop values.
+    fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=39.688699&lon=-86.06519`, {
     method: 'GET',
     headers: new Headers ({
       'Accept': 'application/json',
@@ -11,14 +17,27 @@ const Zomato = (props) => {
     })
   }).then( (res) => res.json())
   .then((data) => {
-    console.log(data);
-  })   
+    const resturants = data.nearby_restaurants.slice(0 ,3)
+    //console.log(resturants[0]); 
+    setRestOne(resturants[0].restaurant.name)
+    setRestTwo(resturants[1].restaurant.name)
+    setRestThree(resturants[2].restaurant.name) 
+    console.log("RestOne",restOne)
+    console.log("RestTwo",restTwo)
+    console.log("RestThree",restThree)
+ })  
+ 
 }
+useEffect(() => {
+  fetchZomato();
+}, [])
   
   return (
-
     <div>
+     Test
     </div>
+
+    
 
   );
 }
