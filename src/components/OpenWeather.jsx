1c983b3ab30d159073b76b6624e10f01d86aa9ed
Raina86 <you@example.com>
2020-10-26 20:30:20 -0400

    api

--- src/components/OpenWeather.jsx
+++ src/components/OpenWeather.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function OpenWeather() {
-  return (
+function OpenWeather(props) {
+ const[temperatue, setTemperature] =useState('');
+const fetchOpenWeather=()=>{
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=39.688699&lon=-86.06519&units=imperial&appid=a5e392dda217cc611a0949968b1a6f0f`)
+ .then(res=> res.json())
+ .then(res =>{
+   console.log (res)
+   
+ })
+}
+useState(()=>{
+  fetchOpenWeather()
+})
+  return ( 
 
     <div>
+     
     </div>
 
   );
 }
 
+ 
 export default OpenWeather;
